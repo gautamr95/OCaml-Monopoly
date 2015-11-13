75c0d94317e13f8565a1165cfbd1d52072ece09a
Sacheth Hegde <sacheg3@gmail.com>
2015-11-13 04:17:59 +0000

    Added GUI functions

---
+++ Gui_functions.mli
@@ -0,0 +1,6 @@
+open Player_functions
+open Board_functions
+
+(* draw_gui [b pr pl] updates the GUI with the current board b, property
+list pr, and player list pl *)
+val draw_gui : board -> property list -> player list -> ()
