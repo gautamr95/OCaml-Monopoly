191cdc0da8724b3a72f617dd97775e58c623937e
Gaurab Bhattacharya <gb358@cornell.com>
2015-11-30 17:24:20 -0500

    some player functions

--- Player_functions.mli
+++ Player_functions.mli
@@ -4,45 +4,37 @@ open Board_functions
 type player
 
 
-(*generates string to roll dice, called by player in top level*)
-val roll_dice : unit -> string
-
 (*take in property that player is currently on, take in list of owned properties,
   take in player info structure.
   Buys property if not taken and player has enough money, subtracts price from
   player's current savings*)
-val buy_property : property -> property list -> player -> string
 
 (*requests a trade with the selected player*)
-val request_trade : player -> player -> string
 
 (*accepts an incoming trade request from a player*)
-val accept_trade : player -> player -> string
 
 (*declines an incoming trade request from a player*)
-val decline_trade : player -> player -> string
 
 (*upgrades a property owned by a player if they have enough money
   and if the property can be upgraded*)
-val upgrade_property : property -> player -> string
 
 (*ends a players turn and returns the updated player state*)
-val end_turn : unit -> player
+
 
 (*quits the game*)
-val quit : unit -> string
 
 (*gets player money*)
-val savings : player -> string
-
-(*gets turns*)
-val turns : player -> string
-
-(*gets scores*)
-val scores : player -> string
+val get_money : player -> int
 
 (*gets owned properties*)
-val get_properties : player -> string
+val get_pl_property : player -> property list
 
 (*gets current location*)
-val get_location : player -> string
+val get_location : player -> property
+
+val set_money : player -> int -> player
+
+val has_property : player -> property -> bool
+
+val set_property : player -> property -> player
+
