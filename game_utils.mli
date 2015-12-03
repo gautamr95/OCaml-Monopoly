3b997c25d35f1995c3a512b0f87f721eea83be04
gb358 <gb358@cornell.edu>
2015-12-03 17:30:27 -0500

    cleaning and adding buy houses

--- game_utils.mli
+++ game_utils.mli
@@ -47,17 +47,22 @@ val is_ai : board -> int -> bool
 (*take in board,  and id of player to get it, and id of player to lose it*)
 val move_property : board -> int -> int option -> property -> unit
 
+(*get a property's price*)
 val get_prop_price : property -> int
 
+(*get a property's name*)
 val get_prop_name : property -> string
 
 (*take in player id return if the player is in jail, will change to something else tho*)
 val in_jail : board -> int -> bool
 
+(*returns if a position is chance tile*)
 val is_chance : board -> int -> bool
 
+(*returns if a position is a chest tile*)
 val is_chest : board -> int -> bool
 
+(*returns if a tile is jail*)
 val is_go_jail : board -> int -> bool
 
 (*take in player id*)
@@ -70,28 +75,43 @@ val get_pl_prop_of_color: board -> int -> property -> property list ref
 (*get players money from id*)
 val get_money : board -> int -> int
 
-(*takes in position*)
+(*takes in position and returns its tile*)
 val get_tile : board -> int -> tile
 
+(*takes in player id and and return a boolean for if a player is bankrupt*)
 val is_bankrupt : board -> int -> bool
 
+(*returns if other players are all bankrupt*)
 val others_bankrupt : board -> int -> bool
 
-
-
+(*returns rent for a property*)
 val get_rent : property -> int
 
+(*returns holder of a property, in the form of a player id, if its not held
+  return none*)
 val get_holder : property -> int option
 
+(*get the number of houses on a property*)
 val get_houses : property -> int
 
+(*based on position return the tile of that position*)
 val get_tile : board -> int -> tile
+
+(*move player to jail with a given player id, modify their position and set
+  in_jail to true*)
+val move_to_jail : board -> int -> unit
+
+(*print all of a players properties*)
+val print_players_properties : board -> int -> unit
+
+
 (* Creates a board to be used in the beginning
   Inputs:
   - number of human players
   - list of names of the human players (ids will be generated in this order)
   -
-*)
+
 val create_board : int -> string array -> string list -> string -> board
 
-val move_to_jail : board -> int -> unit
+
+*)
