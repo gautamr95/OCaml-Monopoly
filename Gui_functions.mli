2887b8518c92ceb352294750cb1cccbcbe5eac28
tincanman <tiansky.yao95@gmail.com>
2015-12-03 18:04:23 -0500

    Added print_to_cmd to MLI file

--- Gui_functions.mli
+++ Gui_functions.mli
@@ -4,3 +4,8 @@ open Board_functions
 (* draw_gui [b pr pl] updates the GUI with the current board b, property
 list pr, and player list pl *)
 val draw_gui : board -> property list -> player list -> ()
+
+(* print_to_cmd [str] prints the [str] to the command display inside the GUI
+  preconditions: None
+  postconditions: None*)
+val print_to_cmd : str -> ()
