123cee0d01a8cbe52bc6e73e3b00ba83930f2cec
tincanman <tiansky.yao95@gmail.com>
2015-12-05 01:28:53 -0500

    Changed toplevel to use game main

--- toplevel.ml
+++ toplevel.ml
@@ -1,6 +1,6 @@
 open Game_utils
 
 let guit = Thread.create (Gui.main) ()
-let gamet = Thread.create (Gamethread.get_input) ()
+let gamet = Thread.create (Game_main.get_input) ()
 
 let _ = (Thread.join guit, Thread.join gamet)
